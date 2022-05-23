1c048837261c8e9ba7654e1e0c4a38cc77143319
Liheng Yao <ylhs3aw335@hotmail.com>
2022-05-23 12:19:14 +0800

    1. Add some toast to give more info.

--- app/src/main/java/com/liven/demo/ui/home/FoodListAdapter.kt
+++ app/src/main/java/com/liven/demo/ui/home/FoodListAdapter.kt
@@ -2,6 +2,7 @@ package com.liven.demo.ui.home
 
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.liven.demo.R
@@ -19,9 +20,14 @@ class FoodListAdapter(data: MutableList<Food>, val method: (Food, Int, Int) -> U
         holder.getView<Button>(R.id.change_button).setOnClickListener {
             val foodAmount =
                 holder.getView<EditText>(R.id.food_amount).text.toString().toIntOrNull() ?: 0
-            val customerNo =
-                holder.getView<EditText>(R.id.customer_number).text.toString().toIntOrNull()?:1
+            if (foodAmount > 0) {
+                val customerNo =
+                    holder.getView<EditText>(R.id.customer_number).text.toString().toIntOrNull()
+                        ?: 1
                 method.invoke(item, foodAmount, customerNo)
+            } else {
+                Toast.makeText(context, "Please enter the food amount!", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
